cc012e9b5b6f202fa3fc0a96039567e30bf07f39
olynexdev <teamolynexdev@gmail.com>
2025-06-25 10:23:38 +0600

    add new svg icons

---
+++ dist/lib/attributeToCamelCase.d.ts
@@ -0,0 +1,1 @@
+export declare function processSvg(svg: string): string;
--- dist/types.d.ts
+++ dist/types.d.ts
@@ -3,3 +3,8 @@ export interface IconProps {
     className?: string;
     style?: React.CSSProperties;
 }
+export interface IconData {
+    tagname: string;
+    icon: string;
+    npm_tag: string;
+}
